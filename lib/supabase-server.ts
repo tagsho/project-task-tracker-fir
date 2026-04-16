688f92a8e26e9a97793a2b80eeb9bf645aafd2ae
tagsho <taguchi.contact@gmail.com>
2026-04-16 16:48:01 +0900

    Update supabase-server.ts

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,5 +1,6 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import { type ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 
 export function createServerSupabaseClient() {
   const cookieStore = cookies()
