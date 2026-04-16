bdac092523b58f31f42f57ea7a03d77a84aebd33
tagsho <taguchi.contact@gmail.com>
2026-04-16 10:56:42 +0900

    静的ページのキャッシュ削除

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 module.exports = nextConfig
+async headers() {
+    return [
+      {
+        source: '/login',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/dashboard/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
+};
